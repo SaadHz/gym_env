92ac3438f0115c3c8c2c6de5014bf828fb748926
SaadHz <saad4q@gmail.com>
2020-05-21 03:14:53 +0300

    Create __init__.py

---
+++ gym_env/__init__.py
@@ -0,0 +1,10 @@
+from gym.envs.registration import register
+
+register(
+    id='env-v0',
+    entry_point='gym_env.envs:EnvEnv',
+)
+register(
+    id='env-extrahard-v0',
+    entry_point='gym_env.envs:FooExtraHardEnv',
+)
